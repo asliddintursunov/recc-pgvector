15f0d1ae052c2713688fbbd1284f8f77650c81d3
asliddintursunov <asliddintursunovfarkhodovich@gmail.com>
2026-06-01 19:14:20 +0500

    prisma setup

---
+++ backend/src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
+
+@Module({
+  imports: [PrismaModule],
+  exports: [],
+  controllers: [],
+  providers: [],
+})
+export class AppModule { }
